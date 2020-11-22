306786123a7ebef0286fedd0ee5c63bf944541d5
elixz196123 <71353800+elixz196123@users.noreply.github.com>
2020-11-21 17:37:06 -0800

    Update teclado.s

--- teclado.s
+++ teclado.s
@@ -1,11 +1,8 @@
-@ primero.s
-@   from thinkingeek.com
-@   http://thinkingeek.com/2013/01/09/arm-assembler-raspberry-pi-chapter-1/
-@   Defines a main function that returns 2 as an exit code.
-@	
-
-	
-	.global main
-	
-main:   mov r0, #2       @ load immediate value 2 into Register r0 
-	bx lr            @ return 2 to Operating Sytem
+#Makefile
+all: teclado
+teclado: teclado.o
+  ld -o $@ $+
+teclado.o: teclado.s
+  as -g -mfpu=vfpv2 -o $@ $<
+clean:
+  rm -vf teclado *.o
